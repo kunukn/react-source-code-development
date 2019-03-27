fe3ea4e9e7376f0d6b1d370a6277642df7799e38
Kunuk Nykjær <kunyk@dfds.com>
2019-03-27 08:33:44 +0100

    remove stuff from Hello

--- src/components/Hello/Hello.jsx
+++ src/components/Hello/Hello.jsx
@@ -2,16 +2,5 @@ import React from 'react';
 import cx from 'classnames';
 import './Hello.css';
 
-const obj = {
-  foo: {
-    bar: {
-      baz: 1,
-    },
-  },
-};
-
-const baz = obj?.foo?.bar?.baz;
-
-
-let Hello = ({children}) => <p className='hello hello--awesome'>{children} {baz}</p>;
+let Hello = ({children}) => <p className='hello hello--awesome'>{children}</p>;
 export default Hello;
